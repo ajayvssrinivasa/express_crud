66d525c2ca550c869bd3b5c272976409d5a84279
ajayvssrinivasa <sriaj711@gmail.com>
2021-12-01 16:29:20 +0530

    weather app

--- App.js
+++ App.js
@@ -1,71 +1,59 @@
-const express = require('express');
-const cookieParser=require('cookie-parser');
-const session=require('express-session');
-const app = express();
-const PORT = 8899;
-const fs = require('fs');
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+const express=require('express');
+const app=express();
+const request=require('request');
+const PORT=5400;
+const weatherUrl="https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=39726c5f566a65a8972cc03ec9f8c4fb"
 
+app.use(express.static('public'));
+
+app.set('views','./src/views');
 
 app.set('view engine','ejs');
 
-app.get("/", (req, res)=>{
-        // let courses = [{course_name:"React", id: 1},
-        // {course_name:"Node", id: 2},{course_name:"Angular", id: 3},{course_name:"Express", id: 4},];
-    const posts = getPostdata();
-    res.render('crud',{posts:posts,data:{}});
-
-})
-app.post('/write', (req,res)=>{
-    const posts = getPostdata();
-    let title = req.body.title;
-    let description = req.body.description;
-    posts.push({title:title, description:description});
-    savePostdata(posts);
-    res.redirect("/");
+app.get('/',(req,res)=>{
+    res.send("API is Working")
 })
 
-app.get('/edit/:title', (req, res)=>{
-    const {title} = req.params;
-    const posts = getPostdata();
-    const data = posts.filter(pt=>pt.title === title);
-    res.render('crud', {posts:posts, data:data[0]});
+function getWeather(url){
+    var options={
+        url:weatherUrl,
+        headers:{
+            'User-Agent':'request'
+        }
+    };
+    return new Promise(function(resolve,reject){
+        request.get(options,function(err,response,body){
+            if(err){
+                reject(err);
+            }else{
+                resolve(body);
+            }
+        })
+    })
+}
+app.get('/weather',(req,res)=>{
+    var dataPromise = getWeather();
+    dataPromise.then(JSON.parse)
+    .then(function(result){
+        res.render('home',{result,title:"***Weather App***"})
+    })
 })
 
-app.get('/delete/:ind',(req,res)=>{
-    const {ind} = req.params;
-    const posts = getPostdata();
-    posts.splice(ind, 1);
-    savePostdata(posts);
-    res.redirect('/')
+app.get('/weather',(req,res)=>{
+    request(url,(err,response,body)=>{
+        if(err){
+            console.log(err);
+        }else{
+            const output=JSON.parse(body);
+            res.send(output);
+        }
+    })
 })
-app.post('/upd/:title', (req, res)=>{
-    const {title} = req.params;
-    const posts = getPostdata();
-    const index = posts.findIndex(pt=>pt.title === title);
-    posts.splice(index,1);
-    savePostdata(posts);
-    res.redirect(307, "/write")
-})
-app.get("/arrdata",(req, res)=>{
-    let arr = ["Node", "React", "Express", "Mongo"];
-    res.render('array',{arr:arr});
-})
-
-
 
 app.listen(PORT,(err)=>{
-    if(err) throw err;
-    console.log(`Work on ${PORT}`);
+    if(err){
+        console.log('error in the API call');
+    }else{
+        console.log(`App is Running on ${PORT} `);
+    }
 })
-
-const savePostdata = (data) => {
-    const stringifyData = JSON.stringify(data)
-    fs.writeFileSync('Posts.json', stringifyData)
-}
-
-const getPostdata = () => {
-    const jsonData = fs.readFileSync('Posts.json')
-    return JSON.parse(jsonData)    
-}
